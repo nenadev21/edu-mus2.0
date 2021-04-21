e3c33834837716df7cfb49d5728f5b2026551fc2
Dani <69801600+nenadev21@users.noreply.github.com>
2021-04-21 16:20:08 -0400

    Created new component
    Created new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Test from "./test";
 
 function App() {
   return (
     <div className="App">
      
-        <button className="button is-primary is-rounded"> bulma </button>
+      <button className="button is-primary is-rounded"> bulma </button>
+      <Test />
         
       
     </div>
