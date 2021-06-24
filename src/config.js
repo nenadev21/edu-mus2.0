ef5552fff5b57efdd67eebb7cf728993cfa0b81f
Arit Oyekan <aaoyekanmi4@gmail.com>
2021-06-23 19:51:33 -0500

    changed to https

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  API_BASE_URL: 'http://aqueous-hollows-12270.herokuapp.com' || 'http://localhost:8000'
+  API_BASE_URL: 'https://aqueous-hollows-12270.herokuapp.com' || 'http://localhost:8000'
 };
