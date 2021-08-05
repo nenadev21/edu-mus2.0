1eccca126389600a46578273f98d344b37a992c3
Arit Oyekan <aaoyekanmi4@gmail.com>
2021-08-05 00:18:38 -0500

    fixed config to use endpoint when deployed

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT 
+  REACT_APP_API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT || 'https://infinite-hollows-82154.herokuapp.com'
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './Home.css';
-import { Link } from 'react-router-dom';
 import UnitCard from '../../components/cards/UnitCard';
 import HomeHeader from '../../components/headers/HomeHeader';
 
 import { LevelAndUnitContext } from '../../context/LevelAndUnitContext';
 
+
 export default function Home() {
     const { getAllLevels, getAllUnits, levels, units } = useContext(LevelAndUnitContext);
+    const [filteredUnits, setFilteredUnits] = useState(units)
 
     useEffect(() => {
       getAllLevels();
@@ -15,19 +16,32 @@ export default function Home() {
       // eslint-disable-next-line
   }, []);
 
-  const renderUnits = (units) => {
-    return units.map((unit, index) => {
-      return <UnitCard unit={unit} key={index} />;
-    });
-  };
+const filterUnits = (level) => {
+    let selectedUnits = units.filter((type) => type.level_id === level);
+    // console.log(selectedUnits)
+    setFilteredUnits(selectedUnits);
+}
+
+const renderUnits = (units) => {
+    return filteredUnits.map((unit, index) => {
+      return <UnitCard unit={unit} key={index} />
+    })
+  } 
+
+const handleSelection = (e) => {
+  let selectedLevel = parseInt(e.target.value);
+  // console.log(selectedLevel)
+  filterUnits(selectedLevel)
+  //how can I display correct units based on this filter?
+} 
+
+
+
   //this function maps over the array of levels. The index intends to assign an unique id to each element. This is using an "arrow function"
   const renderLevels = (levels) => {
     return levels.map((level, index) => {
       return (
-        <li key={index}>
-          {' '}
-          <Link to="/"> {level.level_name} </Link>{' '}
-        </li>
+        <li value={level.id} key={index} onClick={handleSelection}>{level.level_name}</li>
       );
     });
     };
