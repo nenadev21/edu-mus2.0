c15127013ec62ffee6d13ef5b5b72ab809b323c5
Dani <dvera1@babson.edu>
2022-10-13 10:36:04 -0400

    Fixed resource picture

--- src/data/resources.js
+++ src/data/resources.js
@@ -1,38 +1,38 @@
 export const categories = [
-  'Cantar',
-  'Vocalizar',
-  'Entonar',
-  'Escalas',
-  'Ritmo',
-  'Partituras',
-  'Musica Local',
+  "Cantar",
+  "Vocalizar",
+  "Entonar",
+  "Escalas",
+  "Ritmo",
+  "Partituras",
+  "Musica Local",
 ];
 export const resources = [
   {
     resource_img:
-      'https://1.bp.blogspot.com/-Ounb4wAJXGc/XBCPaG0tT-I/AAAAAAAAINs/QfPlez-b2g0oMeXaLCfEaMNo8Zc3lZEfACLcBGAs/w1200-h630-p-k-no-nu/isaac.PNG',
-    resource_title: 'Juanito el Aymara',
+      "https://1.bp.blogspot.com/-Ounb4wAJXGc/XBCPaG0tT-I/AAAAAAAAINs/QfPlez-b2g0oMeXaLCfEaMNo8Zc3lZEfACLcBGAs/w1200-h630-p-k-no-nu/isaac.PNG",
+    resource_title: "Juanito el Aymara",
     resource_description:
-      'Video animado donde Juanito explica como su comunidad vive la musica',
+      "Video animado donde Juanito explica como su comunidad vive la música",
   },
   {
     resource_img:
-      'https://docplayer.es/docs-images/52/30423940/images/12-0.jpg',
-    resource_title: 'Musica Mapuche',
+      "https://docplayer.es/docs-images/52/30423940/images/12-0.jpg",
+    resource_title: "Música Mapuche",
     resource_description:
-      'Mini documental que relata la vida de una familia en la comunidad mapuche',
+      "Mini documental que relata la vida de una familia en la comunidad mapuche",
   },
   {
-    resource_img: 'https://i.ytimg.com/vi/6N3wRfy9w3Q/mqdefault.jpg',
-    resource_title: 'Latinoamerica',
+    resource_img: "https://i.ytimg.com/vi/6N3wRfy9w3Q/mqdefault.jpg",
+    resource_title: "Latinoamerica",
     resource_description:
-      'Video que explica los diferentes estilos de la musica en Chile, Bolivia y Peru',
+      "Video que explica los diferentes estilos de la música en Chile, Bolivia y Perú",
   },
   {
     resource_img:
-      'https://noticiaspositivas.org/wp-content/uploads/2018/11/Programa-262F11-CCK-1024x480.jpg',
-    resource_title: 'Musica y Emociones',
+      "https://www.cautionspoilers.com/wp-content/uploads/2018/11/Screen-Shot-2018-11-30-at-16.14.21-300x168.png",
+    resource_title: "Música y Emociones",
     resource_description:
-      'Corto sobre un nino enfermo. Ejemplifica como la musica nos ayuda a expresar emociones',
+      "Corto sobre un niño enfermo. Ejemplifica cómo la música nos ayuda a expresar emociones",
   },
 ];
